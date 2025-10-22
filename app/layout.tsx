d5ae56313c96a16c02f4564cf6c64367c1bea77e
NonOwnersNow <miles@ableautoinsurance.com>
2025-10-22 09:19:02 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = { title: "Able Quote Mockups" };
+
+import "./globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
